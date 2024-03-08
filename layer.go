82594b599c226cee391dc8c7b62eef2f4b0bc22e
Ishan Goel <igoel.mail@gmail.com>
2024-03-08 01:01:08 -0500

    lots of stuff

--- layer.go
+++ layer.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math"
 	"math/rand"
 )
@@ -13,7 +14,6 @@ type layer struct {
 	activations []float64   // activations for each neuron
 	zs          []float64   // weighted sum for each neuron + bias
 	//input       []float64
-
 	//nextLayer *layer
 	//prevLayer *layer
 }
@@ -45,17 +45,27 @@ func randomWeights(inputSize, thisSize int) [][]float64 {
 func randomBiases(size int) []float64 {
 	biases := make([]float64, size)
 	for i := range biases {
-		biases[i] = rand.Float64()*2 - 1
+		//biases[i] = (rand.Float64()*2 - 1) * 0
+		biases[i] = 0
 	}
 	return biases
 }
 
+//func activate(x float64) float64 {
+//	return 1 / (1 + math.Exp(-x))
+//}
+//
+//func activatePrime(x float64) float64 {
+//	return activate(x) * (1 - activate(x))
+//}
+
+// try tanh()
 func activate(x float64) float64 {
-	return 1 / (1 + math.Exp(-x))
+	return math.Tanh(x)
 }
 
 func activatePrime(x float64) float64 {
-	return activate(x) * (1 - activate(x))
+	return 1 - math.Tanh(x)*math.Tanh(x)
 }
 
 func costFunc(err float64) float64 {
@@ -66,19 +76,22 @@ func costPrime(err float64) float64 {
 	return 2 * err
 }
 
-type network []*layer
+type network struct {
+	layers []*layer
+	cost   float64
+}
 
-func (n network) forward(input []float64) []float64 {
-	n[0].activations = input // set the input layer's activations
-	for i := range n {
+func (n *network) forward(input []float64) []float64 {
+	n.layers[0].activations = input // set the input layer's activations
+	for i := range n.layers {
 		if i == 0 {
 			continue
 		}
 		//fmt.Println("forwarding layer", i, "with input", n[i-1].activations, "weights", n[i].weights, "biases", n[i].biases)
-		n[i].forward(input)
-		input = n[i].activations
+		n.layers[i].forward(input)
+		input = n.layers[i].activations
 	}
-	return n[len(n)-1].activations
+	return n.layers[len(n.layers)-1].activations
 }
 
 func (l *layer) forward(input []float64) {
@@ -92,39 +105,43 @@ func (l *layer) forward(input []float64) {
 	}
 }
 
-func (n network) backward(wanted []float64) {
-
-	rate := 0.1
-
-	delta := make([]float64, len(wanted)) // delta[j] is delC / delZ[j]
-	for i, activation := range n[len(n)-1].activations {
-		delta[i] = 2 * costPrime(activation-wanted[i]) * activatePrime(n[len(n)-1].zs[i]) // delC / delZ
-		updateLayerWithDelta(n[len(n)-1], n[len(n)-2], delta, rate)
+func (n *network) backward(wanted []float64) {
+	n.cost = 0.0
+	for i, activation := range n.layers[len(n.layers)-1].activations {
+		n.cost += costFunc(activation - wanted[i])
 	}
 
-	for i := len(n) - 2; i >= 1; i-- { // for each layer except the input and output
-		newdelta := make([]float64, len(n[i].activations)) // one for each neuron
+	rate := 0.01
+
+	delta := make([]float64, len(wanted)) // delta[j] is delC / delZ[j]
+	for i, activation := range n.layers[len(n.layers)-1].activations {
+		delta[i] = 2 * costPrime(activation-wanted[i]) * activatePrime(n.layers[len(n.layers)-1].zs[i]) // delC / delZ
+		updateLayerWithDelta(n.layers[len(n.layers)-1], n.layers[len(n.layers)-2], delta, rate)
+	}
+
+	for i := len(n.layers) - 2; i >= 1; i-- { // for each layer except the input and output
+		newdelta := make([]float64, len(n.layers[i].activations)) // one for each neuron
 		// go over all the neurons in the next layer and add up the impact of their delta on this layer's delta
-		for j := range n[i].activations { // for each neuron in this layer
+		for j := range n.layers[i].activations { // for each neuron in this layer
 			var sum float64
-			for k := range n[i+1].activations { // for each neuron in the next layer
-				sum += n[i+1].weights[k][j] * delta[k]
+			for k := range n.layers[i+1].activations { // for each neuron in the next layer
+				sum += n.layers[i+1].weights[k][j] * delta[k]
 			}
-			newdelta[j] = sum * activatePrime(n[i].zs[j])
+			newdelta[j] = sum * activatePrime(n.layers[i].zs[j])
 		}
-		updateLayerWithDelta(n[i], n[i-1], newdelta, rate)
+		updateLayerWithDelta(n.layers[i], n.layers[i-1], newdelta, rate)
 		delta = newdelta
 	}
 }
 
-func updateLayerWithDelta(l *layer, lp *layer, delta []float64, learnRate float64) {
+func updateLayerWithDelta(l *layer, lp *layer, delta []float64, learnStepRate float64) {
 	for i, weights := range l.weights {
 		for j := range weights {
-			l.weights[i][j] -= learnRate * errorToDelCDelWeight(delta[i], lp.activations[j])
+			l.weights[i][j] -= learnStepRate * errorToDelCDelWeight(delta[i], lp.activations[j])
 		}
 	}
 	for i := range l.biases {
-		l.biases[i] -= learnRate * errorToDelCDelBias(delta[i])
+		l.biases[i] -= learnStepRate * errorToDelCDelBias(delta[i])
 	}
 }
 
@@ -137,4 +154,14 @@ func errorToDelCDelWeight(delta, prevActive float64) float64 {
 // delta should be the error for the neuron that this bias is for
 func errorToDelCDelBias(delta float64) float64 {
 	return delta
+}
+
+func (n *network) String() string {
+	result := ""
+	for i, layer := range n.layers {
+		result += "Layer " + fmt.Sprint(i) + ":\n"
+		result += "Weights: " + fmt.Sprint(layer.weights) + "\n"
+		result += "Biases: " + fmt.Sprint(layer.biases) + "\n\n"
+	}
+	return result
 }
--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 
-	n := network{
-		newRandomLayer(0, 1), // input layer
-		newRandomLayer(1, 4),
-		newRandomLayer(4, 4),
-		newRandomLayer(4, 1),
+	n := &network{
+		layers: []*layer{
+			newRandomLayer(0, 1), // input layer
+			newRandomLayer(1, 4),
+			newRandomLayer(4, 4),
+			newRandomLayer(4, 4),
+			//newRandomLayer(4, 5),
+			newRandomLayer(4, 1),
+		},
 	}
 
 	//fmt.Println(n[1].weights)
@@ -16,14 +23,50 @@ func main() {
 	input := []float64{1}
 	fmt.Println(n.forward(input))
 
-	// backpropagation
-	for i := 0; i < 100_000; i++ {
-		for d := 0.0; d < 1; d += 0.01 {
-			input := []float64{d}
-			target := []float64{d * d}
-			n.forward(input)
-			n.backward(target)
+	fmt.Println(n)
+
+	// train
+	lastCost := -1.0
+	inputs, targets := makeTrainingDataX2()
+	for i := 0; i < 1_000_000; i++ {
+		//for d := 0.0; d < 1; d += 0.01 {
+		//	//input := []float64{d}
+		//	//target := []float64{math.Sin(d * 2 * math.Pi)}
+		//	//target := []float64{d * d}
+		//	//if input[0] > 0.5 {
+		//	//target[0] = input[0]
+		//	//}
+		//	n.forward(input)
+		//	n.backward(target)
+		//}
+		for i := range inputs {
+			n.forward(inputs[i])
+			n.backward(targets[i])
 		}
+
+		if i%1000 == 0 {
+			fmt.Println("iteration", i, "cost", n.cost)
+			if lastCost > 0 {
+				fmt.Println("cost ratio", n.cost/lastCost)
+				fmt.Println("improvement", 1/n.cost-1/lastCost)
+				fmt.Println("Score", 1/n.cost)
+			}
+			lastCost = n.cost
+
+			//for d := 0.0; d < 1; d += 0.01 {
+			//	input := []float64{d}
+			//	fmt.Println(input[0], ",", n.forward(input)[0])
+			//	//fmt.Println(n.forward(input)[0])
+			//}
+			//if i > 0 {
+			//	return
+			//}
+		}
+
+		//if n.cost < 1e-10 {
+		//	fmt.Println("Found a good enough solution")
+		//	break
+		//}
 
 		//input := []float64{0.5}
 		//target := []float64{0.5}
@@ -36,11 +79,63 @@ func main() {
 		//n.backward(target)
 	}
 
-	for d := 0.0; d < 1; d += 0.01 {
+	for d := -1.0; d < 1; d += 0.005 {
 		input := []float64{d}
-		fmt.Println(input, n.forward(input)[0])
+		fmt.Println(input[0], ",", n.forward(input)[0])
 		//fmt.Println(n.forward(input)[0])
 	}
 
-	//fmt.Println(n[1].weights)
+	fmt.Println("Score", 1/n.cost)
+
+	fmt.Println(n.layers[1].weights)
+
+	fmt.Println(n)
+}
+
+func oneHot(n int, i int) []float64 {
+	out := make([]float64, n)
+	out[i] = 1.0
+	return out
+}
+
+func getIndexWithMaxValue(arr []float64) int {
+	max := 0.0
+	index := 0
+	for i, v := range arr {
+		if v > max {
+			max = v
+			index = i
+		}
+	}
+	return index
+}
+
+func makeTrainingDataSin() (inputs, targets [][]float64) {
+	inputs = make([][]float64, 0, 100)
+	targets = make([][]float64, 0, 100)
+	for d := -1.0; d < 1; d += 0.01 {
+		inputs = append(inputs, []float64{d})
+		targets = append(targets, []float64{math.Sin(d * 2 * math.Pi)})
+	}
+	return
+}
+
+func makeTrainingDataX2() (inputs, targets [][]float64) {
+	inputs = make([][]float64, 0, 100)
+	targets = make([][]float64, 0, 100)
+	for d := -1.0; d < 1; d += 0.01 {
+		inputs = append(inputs, []float64{d})
+		targets = append(targets, []float64{d * d})
+	}
+	return
+}
+
+func makeTrainingDataNegX() (inputs, targets [][]float64) {
+	inputs = make([][]float64, 0, 100)
+	targets = make([][]float64, 0, 100)
+	for d := -1.0; d < 1; d += 0.01 {
+		inputs = append(inputs, []float64{d})
+		targets = append(targets, []float64{-d})
+	}
+	return
 }
